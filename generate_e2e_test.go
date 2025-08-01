a1b5dce3925d83e4f165363bba2797c0442a5e02
Kaiqi Zheng <kaiqizheng@google.com>
2025-08-01 14:45:21 -0400

    small refactor in e2e test

--- generate_e2e_test.go
+++ generate_e2e_test.go
@@ -29,6 +29,7 @@ import (
 const (
 	repo               = "repo"
 	localRepoBackupDir = "testdata/e2e/generate/repo_backup"
+	APISourceRepo      = "apisource"
 	localAPISource     = "testdata/e2e/generate/api_root"
 )
 
@@ -52,11 +53,11 @@ func TestRunGenerate(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			workRoot := filepath.Join(t.TempDir())
 			repo := filepath.Join(workRoot, repo)
-			APISouceRepo := filepath.Join(workRoot, localAPISource)
+			APISourceRepo := filepath.Join(workRoot, localAPISource)
 			if err := prepareTest(t, repo, workRoot, localRepoBackupDir); err != nil {
 				t.Fatalf("languageRepo prepare test error = %v", err)
 			}
-			if err := prepareTest(t, APISouceRepo, workRoot, localAPISource); err != nil {
+			if err := prepareTest(t, APISourceRepo, workRoot, localAPISource); err != nil {
 				t.Fatalf("APISouceRepo prepare test error = %v", err)
 			}
 
@@ -68,7 +69,7 @@ func TestRunGenerate(t *testing.T) {
 				fmt.Sprintf("--api=%s", test.api),
 				fmt.Sprintf("--output=%s", workRoot),
 				fmt.Sprintf("--repo=%s", repo),
-				fmt.Sprintf("--api-source=%s", APISouceRepo),
+				fmt.Sprintf("--api-source=%s", APISourceRepo),
 			)
 			cmd.Stderr = os.Stderr
 			cmd.Stdout = os.Stdout
