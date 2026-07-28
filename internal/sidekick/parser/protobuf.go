94159019fdca7be5a58530fd32f39999db871140
Carlos O'Ryan <coryan@google.com>
2026-07-28 17:14:11 -0400

    feat(sidekick): capture C#, PHP, and Ruby file options (#7092)
    These options contain alternative names for the package or namespace
    containing all the types for an API. We will use these to emit warnings
    in Swift when the default name does not match the overrides for these
    languages.
    
    The idea is that C#, PHP, and Ruby better capture the intent of some
    human on how to style `secretmanager`: `SecretManager` is better, but
    the generator cannot know without help.
    
    The person on the librarian rotation can silence the warnings by setting
    the name override.

--- internal/sidekick/parser/protobuf.go
+++ internal/sidekick/parser/protobuf.go
@@ -328,6 +328,21 @@ func makeAPIForProtobuf(serviceConfig *serviceconfig.Service, req *pluginpb.Code
 		var fileServices []*api.Service
 		fFQN := "." + f.GetPackage()
 
+		if value, err := protobufUpdateFileOption(result.CsharpNamespace, f.GetOptions().GetCsharpNamespace()); err == nil {
+			result.CsharpNamespace = value
+		} else {
+			return nil, err
+		}
+		if value, err := protobufUpdateFileOption(result.PhpNamespace, f.GetOptions().GetPhpNamespace()); err == nil {
+			result.PhpNamespace = value
+		} else {
+			return nil, err
+		}
+		// TODO(https://github.com/googleapis/librarian/issues/7091) - also use protobufUpdateFileOption() for Ruby
+		if result.RubyPackage == "" {
+			result.RubyPackage = f.GetOptions().GetRubyPackage()
+		}
+
 		// Messages
 		for _, m := range f.MessageType {
 			mFQN := fFQN + "." + m.GetName()
@@ -476,6 +491,24 @@ var descriptorpbToTypez = map[descriptorpb.FieldDescriptorProto_Type]api.Typez{
 	descriptorpb.FieldDescriptorProto_TYPE_GROUP:    api.TypezGroup,
 	descriptorpb.FieldDescriptorProto_TYPE_MESSAGE:  api.TypezMessage,
 	descriptorpb.FieldDescriptorProto_TYPE_ENUM:     api.TypezEnum,
+}
+
+// protobufUpdateFileOption validates an update of `current` to `got`.
+//
+// This returns an error if `current` != "" and `current` != got.
+func protobufUpdateFileOption(current, got string) (string, error) {
+	if current == "" {
+		return got, nil
+	}
+	if current == got {
+		return got, nil
+	}
+	// The Well-Known Types have inconsistent annotations for C#. Just ignore
+	// the inconsistency, it is not a real problem.
+	if got == "" || strings.HasPrefix(got, "Google.Protobuf.") {
+		return current, nil
+	}
+	return "", fmt.Errorf("mismatched file option value, want=%s got=%s", current, got)
 }
 
 func normalizeTypes(model *api.API, in *descriptorpb.FieldDescriptorProto, field *api.Field) {
